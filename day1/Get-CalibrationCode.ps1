6881094721da4cadeb219d5480fa8d1d21232aa9
dtrac <dtracey@hotmail.co.uk>
2023-12-02 18:46:48 +0000

    day1-part2

--- day1/Get-CalibrationCode.ps1
+++ day1/Get-CalibrationCode.ps1
@@ -36,14 +36,25 @@ $calibrationValue = $null
 
 # Loop through each line in the input file to calculate the calibration value
 foreach ($line in $inputFileContent) {
+
+    Write-Verbose "`$[original]line: $line"
+    # Replace any number words with digits
+    try {
+        $line = Replace-FirstLastNumberWordsWithDigits $line
+    }
+    catch {
+        Write-Error "error with $line"
+    }
+    
     # Get the first and last numbers from the line and calculate the line value
     $lineValue = (Get-FirstNumber $line) + (Get-LastNumber $line)
     
     # Output details for each line (Verbose output)
-    Write-Verbose "`$line: $line`n`$lineValue: $lineValue`n`$runningTotal: $calibrationValue"
+    Write-Verbose "`$[changed]line: $line`n`$lineValue: $lineValue`n`$runningTotal: $calibrationValue"
     
     # Calculate the running total (calibration value)
     $calibrationValue = $calibrationValue += [int]$lineValue
+    Write-Verbose "`$runningTotal: $calibrationValue`n"
 }
 
 # Output the final calibration value
@@ -66,4 +77,61 @@ function Get-LastNumber ($inputString) {
         }
     }
     return $lastNumber
+}
+
+<# function Replace-NumberWordsWithDigits ($inputString) {
+
+    $numberWordsToDigits = @{
+        'one' = '1'
+        'two' = '2'
+        'three' = '3'
+        'four' = '4'
+        'five' = '5'
+        'six' = '6'
+        'seven' = '7'
+        'eight' = '8'
+        'nine' = '9'
+    }
+
+    $numberWordsToDigits.Keys | ForEach-Object { 
+        $inputString = $inputString.Replace($_, $numberWordsToDigits["$_"])   
+    }
+    return $InputString
+} #>
+
+function Replace-FirstLastNumberWordsWithDigits ($inputString) {
+
+    $numberWordsToDigits = @{
+        'one' = '1'
+        'two' = '2'
+        'three' = '3'
+        'four' = '4'
+        'five' = '5'
+        'six' = '6'
+        'seven' = '7'
+        'eight' = '8'
+        'nine' = '9'
+    }
+
+    $firstIndex = $null
+    $lastIndex = $null
+
+    $numberWordsToDigits.Keys | ForEach-Object {
+        $numberWord = $_
+        $index = $inputString.IndexOf($numberWord)
+        if ($index -ge 0) {
+            if (-not $firstIndex) {
+                $firstIndex = $index
+                $inputString = $inputString.Substring(0, $index) + $numberWordsToDigits[$numberWord] + $inputString.Substring($index + $numberWord.Length)
+            }
+            $lastIndex = $index
+        }
+    }
+
+    $lastNumberWord = $numberWordsToDigits.Keys | Where-Object { $inputString.Contains($_) } | Select-Object -Last 1
+    if ($lastIndex -ne $null -and $lastIndex -ne $firstIndex) {
+        $inputString = $inputString.Substring(0, $lastIndex) + $numberWordsToDigits[$lastNumberWord] + $inputString.Substring($lastIndex + $lastNumberWord.Length)
+    }
+
+    return $inputString
 }
